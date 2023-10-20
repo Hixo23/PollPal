6672d9787f6f8701943ec446fcd9395f73bcb5bc
hiko <hixohixo32@gmail.com>
2023-10-20 18:09:23 +0200

    Initial commit

---
+++ src/components/ui/Header/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="w-full h-16 flex items-center">
+      <h1>Nayata</h1>
+    </header>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,20 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      text: "#130e01",
+      background: "#fffaeb",
+      primary: "#ff8400",
+      secondary: "#fff5d6",
+      accent: "#cf4307",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
