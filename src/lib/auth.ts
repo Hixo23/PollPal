97011014aac9496784ed286bd7690bf924f56fa8
hiko <hixohixo32@gmail.com>
2023-10-22 19:23:01 +0200

    fix: add secret in nextauth options

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,10 +2,11 @@ import { NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 export const authOptions: NextAuthOptions = {
+  secret:process.env.NEXT_PUBLIC_NEXTAUTH_secret,
   providers: [
     DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+      clientId: process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID as string,
     }),
   ],
 };
