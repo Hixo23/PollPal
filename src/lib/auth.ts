45d3c6e602d0ed924780d99a0b5f9b1a6e23fc9d
hiko <hixohixo32@gmail.com>
2023-10-22 19:35:45 +0200

    fix: add client sercret in discordprovider options

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,7 +6,7 @@ export const authOptions: NextAuthOptions = {
   providers: [
     DiscordProvider({
       clientId: process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_DISCORD_CLIENT_SECRET as string,
     }),
   ],
 };
