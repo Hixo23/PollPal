4a2498397cab8b15d91df686b2013eea664a6e8f
Hiko <hixohixo32@gmail.com>
2023-11-08 15:45:03 +0100

    feat: add create poll button on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
 import { PollVote } from "@/components/poll/Poll";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen w-[95vw] justify-center overflow-hidden p-4 ">
       <section className="flex flex-col items-center justify-center gap-12 md:flex-row md:justify-between">
-        <h2 className="w-1/2 text-6xl font-bold text-text">
-          Create your poll <span className="text-primary">in seconds!</span>
-        </h2>
+
+        <div className='flex flex-col text-left gap-4'>
+          <h2 className="w-1/2 text-6xl font-bold text-text">
+            Create your poll <span className="text-primary">in seconds!</span>
+          </h2>
+          <Link href={'/add-poll'} className='w-fit py-2 px-4 bg-primary text-text rounded-xl'>Create poll</Link>
+        </div>
         <PollVote
           title="Example poll"
           options={[
